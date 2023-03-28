9abefdbed021ce3a00895d05b6fee8a8be821973
divyakondakrindi <118252095+divyakondakrindi@users.noreply.github.com>
2023-03-28 17:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-//your code here
-setInterval(() => {
-  let a = fun(document.getElementsByTagName("input")[0]);
-  document.getElementsByTagName("div")[0].innerHTML = a;
-}, 10);
-
-fun = (e) => e.value.length;
+const input = document.getElementById('evaluatedText');
+const h3 = document.getElementById('wordCount');
+var content = "";
+input.addEventListener('input',()=>{
+	
+	content = input.value.trim();
+	if(content==""){
+		h3.innerHTML="0";
+	}else{
+		h3.innerHTML = content.split(/\s+/ig).length;
+	}
+	
+	
+	
+})
